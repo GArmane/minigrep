a7b4e833fb95088948e8b67d970b51be0e049db6
Giovanni Armane <giovanniarmane@gmail.com>
2024-10-24 20:25:13 -0300

    Command line args simple parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, process::ExitCode};
+
+fn main() -> ExitCode {
+    let args: Vec<String> = env::args().collect();
+    
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for {query}");
+    println!("In file {file_path}");
+
+    ExitCode::SUCCESS
 }
