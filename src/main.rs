1e05940bb8a34a8bf0c090418fef0ca64ef7e6ee
Giovanni Armane <giovanniarmane@gmail.com>
2024-10-24 21:23:58 -0300

    Separate logic into a lib crate

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
+use minigrep::Config;
 use std::{env, process};
-use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,30 +8,10 @@ fn main() {
         process::exit(1)
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.query);
-
-    let contents = fs::read_to_string(config.file_path)
-        .expect("Should have been able to read the file");
-
-    println!("With text:\n{contents}");
-
-    process::exit(0);
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-        Ok(Config { query, file_path })
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    } else {
+        process::exit(0);
     }
 }
