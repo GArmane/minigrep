6571baa32b1405a465c4a705d8fbba00e19e0013
Giovanni Armane <giovanniarmane@gmail.com>
2024-10-29 03:06:00 -0300

    Refactor call to filter

--- src/lib.rs
+++ src/lib.rs
@@ -59,7 +59,7 @@ fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
     contents
         .lines()
-        .filter(|line| line.to_lowercase().contains(&query))
+        .filter(|&line| line.to_lowercase().contains(&query))
         .collect()
 }
 
